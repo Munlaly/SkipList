bf610740889667968a8fe34f708a6bcb4ec88999
Munlaly <nemetdbence@gmail.com>
2025-04-22 18:20:31 +0300

    project created

---
+++ skip_list.c
@@ -0,0 +1,5 @@
+//
+// Created by Bence on 4/22/2025.
+//
+
+#include "skip_list.h"
---
+++ skip_list.h
@@ -0,0 +1,8 @@
+//
+// Created by Bence on 4/22/2025.
+//
+
+#ifndef SKIP_LIST_H
+#define SKIP_LIST_H
+
+#endif //SKIP_LIST_H
